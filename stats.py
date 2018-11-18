187ccb6f831359965b7af7b82e674a3bbda20882
Anuar Talipov <anuartb@gmail.com>
2018-11-18 19:23:55 +0900

    Added script for making simple statistics of data

---
+++ stats.py
@@ -0,0 +1,19 @@
+#A simple script for counting the percentage and number 
+#of missing values in the data
+import csv
+
+file_name = "data_train.csv"
+
+with open(file_name, "r") as csvfile:
+	rd = csv.reader(csvfile)
+	total = [0 for i in range(24)]
+	miss = [0 for i in range(24)]
+	for row in rd:
+		for i, it in enumerate(row):
+			total[i] += 1
+			if not it:
+				miss[i] += 1
+	with open("")
+	for i in range(23):
+		print("The col #{} percentage is: {}".format(i, miss[i] / total[i]))
+		print("The col #{} number is: {}".format(i, miss[i]))
