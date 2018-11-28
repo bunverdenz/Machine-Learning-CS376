cd3f0d200ae34f906854d9e611424f1668652798
Azamat Smagulov <s.a.smagulov@gmail.com>
2018-11-28 16:06:45 +0900

    modify demo_test.py

--- data/README.md
+++ data/README.md
@@ -1,10 +1,10 @@
 # How to run a specific model
 
- Open the demo_test.py and uncomment specific model lines, then run the following command:
+ Open the demo_test.py and choose the specific model, then run the following command:
 ```sh
 $ python3 demo_test.py
 ```
 
 You can also:
   - Change the indexes of features that are used for training
-  - Uncomment the "save(Y_pred)" line ti save the answer in .csv file
+  - Uncomment the "save(Y_pred)" line to save the answer in .csv file
--- data/demo_test.py
+++ data/demo_test.py
@@ -4,23 +4,27 @@ from demo_preprocess_test import pre_process
 from y_to_csv import save
 from demo_train import xgboost
 
+# sklearn Linear Regression
+def run_sklearn_linear_regression(indexes):
+	regr = sklearn_linear(indexes)
+	Y_pred = regr.predict(x_test)
+	return Y_pred
+
+# sklearn Gradient Boosting Regression
+def run_sklearn_gradient_boosting(indexes):
+	clf = sklearn_gradient_boosting(indexes)
+	Y_pred = clf.predict(x_test)
+	return Y_pred
+
+# xgboost regressor
+def run_xgboost(indexes):
+	model = xgboost(indexes)
+	Y_pred = model.predict(x_test)
+	return Y_pred
+
 #indexes = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
 indexes = [0,8,10,11,12,13,14,15,16,18,19]
 x_test = pre_process(indexes)
 
-# sklearn Linear Regression
-#regr = sklearn_linear(indexes)
-#Y_pred = regr.predict(x_test)
-#save(Y_pred)
-
-
-# sklearn Gradient Boosting Regression
-#clf = sklearn_gradient_boosting(indexes)
-#Y_pred = clf.predict(x_test)
-#save(Y_pred)
-
-
-# xgboost clasifier
-model = xgboost(indexes)
-Y_pred = model.predict(x_test)
+Y_pred = run_xgboost(indexes)
 #save(Y_pred)
